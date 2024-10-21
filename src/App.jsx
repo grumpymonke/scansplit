51a038f05d3ca94e5c9ab751cdb5d80ad17dd4a9
manish` <131983538+grumpymonke@users.noreply.github.com>
2024-10-21 10:54:34 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl">
-      Hello world! 
-    </h1>
-  )
+    <>
+    <h1 className="text-3xl">Hello world </h1>
+    <ButtonGradient />
+    </>
+)
 }
 
 export default App
