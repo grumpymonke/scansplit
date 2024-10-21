03e38b69bdf098a71cb0b9586afce4a23c284439
manish` <131983538+grumpymonke@users.noreply.github.com>
2024-10-21 15:50:42 +0530

    S

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import { useState } from 'react'
-import Logo from './assets/Logo.png'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Button from "./components/Button";
 import Header from "./components/Header";
 
-const App =() =>{
-  return(
+const App = () => {
+  return (
     <>
-   <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        </div>
 
-     </div>
-      <ButtonGradient />
-    
-    
-    
-    </>
-  )
-}
+<ButtonGradient />
+</>
+);
+};
 
-
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header  = () => {
         <div className="fixed top-0 left-0 w-full z-50  bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
         
         <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4"></div>
-          <a className="block w-[12rem] xl:mr-8" href="#hero">
+          <a>
           <img src={Logo} width={190} height={40} alt="Logo" />
           </a>
 <nav className='hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-neutral-800 lg:static lg:flex lg:mx-auto lg:bg-transparent'>
@@ -29,6 +29,15 @@ const Header  = () => {
     ))}
       </div>
     </nav> 
+    <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+        >
+          New account
+        </a>
+        <Button className="hidden lg:flex" href="#login">
+          Sign in
+        </Button>
  </div>
 
     );     
