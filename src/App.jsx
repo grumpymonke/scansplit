396112ed1e47dd51df515239701a862ae8c1df7c
manish` <131983538+grumpymonke@users.noreply.github.com>
2024-10-21 15:53:31 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import Header from "./components/Header";
 const App = () => {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+    
         <h1>hello world
           
         </h1>
-        <Header />
-        </div>
+       
+       
 
 <ButtonGradient />
 </>
