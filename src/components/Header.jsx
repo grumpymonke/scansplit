d81c5d761d7d91d8c8e6940027cb5043304295f5
manish` <131983538+grumpymonke@users.noreply.github.com>
2024-10-24 19:23:29 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {logo} from '../assets/logo.png';
+import { scansplit } from '../assets/scansplit.png';
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <div   className="fixed top-0 left-0 w-full z-50  border-b border-neutral-600 lg:bg-neutral-800 lg:backdrop-blur-sm" > header  
+    <div className='flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4'>
+      
+      <a className='block w-[12rem] xl:mr-8" href="#man'>
+        <img src= {scansplit} width={190} height={40}> alt="scansplit"</img>
+      </a>
+      </div></div>
+     
+    
+      
   )
 }
 
